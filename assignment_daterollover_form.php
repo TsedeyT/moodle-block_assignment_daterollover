5f64befe1336caaacc798aabe8b75cac7024006c
Ts Te <snort.test123@gmail.com>
2014-06-04 23:18:32 +0200

    First commit

--- assignment_daterollover_form.php
+++ assignment_daterollover_form.php
@@ -1,6 +1,32 @@
 <?php
 
+    // This file is part of Moodle - http://moodle.org/
+    //
+    // Moodle is free software: you can redistribute it and/or modify
+    // it under the terms of the GNU General Public License as published by
+    // the Free Software Foundation, either version 3 of the License, or
+    // (at your option) any later version.
+    //
+    // Moodle is distributed in the hope that it will be useful,
+    // but WITHOUT ANY WARRANTY; without even the implied warranty of
+    // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    // GNU General Public License for more details.
+    //
+    // You should have received a copy of the GNU General Public License
+    // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+    /**
+     *  block_assignment_daterollover
+     *
+     *  This plugin adjust assignment date items which includes allowsubmissionfrom,
+     *  due-date, cutoffdate, upcoming events, in a course through one centralized screen  
+     *  rather than having to go into each individual assignment activity.
+     *
+     * @author      Tsedey Terefe <snort.test123@gmail.com>
+     * @license     GNU General Public License version 3
+     * @package     block
+     * @subpackage  assignment_daterollover
+     */
 if (!defined('MOODLE_INTERNAL')) {
     die();
 }
@@ -65,9 +91,9 @@ class assignment_daterollover_form extends moodleform {
 	*required -if the value is not empty, integer 0 is not considered an empty value.
 	*
        **/
-       $mform->addRule('date', null, 'required', null, 'client');
-       $mform->addRule('cutoff', null, 'required', null, 'client');
-       $mform->addRule('allowsubmission', null, 'required', null, 'client');
+       $mform->addRule('date', null, 'required', null, 'server');
+       $mform->addRule('cutoff', null, 'required', null, 'server');
+       $mform->addRule('allowsubmission', null, 'required', null, 'server');
      
  $mform->getSubmitValues();
 
